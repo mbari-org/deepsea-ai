ed08c2ae8c1ba9d3cb5889f515cf96362fd00211
danellecline <dcline@mbari.org>
2022-10-24 12:30:04 -0700

    chore: removed release check

--- deepsea_ai/tests/test_deepsea_ai.py
+++ deepsea_ai/tests/test_deepsea_ai.py
@@ -1,10 +1,3 @@
-from deepsea_ai import __version__
-
-
-def test_version():
-    assert __version__ == "1.3.3"
-
-
 def test_config():
     from deepsea_ai.config.config import Config
     c = Config()
