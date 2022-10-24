0305d0472a4df1de35d0ff6f163bf32385393a32
danellecline <dcline@mbari.org>
2022-10-24 12:08:03 -0700

    test: added role check

--- deepsea_ai/tests/test_deepsea_ai.py
+++ deepsea_ai/tests/test_deepsea_ai.py
@@ -2,4 +2,10 @@ from deepsea_ai import __version__
 
 
 def test_version():
-    assert __version__ == "1.3.2"
+    assert __version__ == "1.3.3"
+
+
+def test_config():
+    from deepsea_ai.config.config import Config
+    c = Config()
+    assert c.get_role() is not None
