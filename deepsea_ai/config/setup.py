fae4a4ab7277d12b453a22ec2114dd4cdb789ce3
danellecline <dcline@mbari.org>
2022-10-25 17:02:38 -0700

    perf: bump default role session duration

--- deepsea_ai/config/setup.py
+++ deepsea_ai/config/setup.py
@@ -112,6 +112,7 @@ def create_role():
     path = '/'
     role_name = 'DeepSeaAI'
     description = 'DeepSeaAI Role'
+    session_mins = 3600*24*7
 
     POLICY_JSON = {
         "Version": "2012-10-17",
@@ -141,7 +142,7 @@ def create_role():
             RoleName=role_name,
             AssumeRolePolicyDocument=json.dumps(POLICY_JSON),
             Description=description,
-            MaxSessionDuration=3600
+            MaxSessionDuration=session_mins
         )
 
         iam.attach_role_policy(
