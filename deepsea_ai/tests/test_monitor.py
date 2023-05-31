828996541566f091a8b1e34280e6e0c24084cb1f
danellecline <dcline@mbari.org>
2023-05-31 13:36:01 -0700

    chore: PEP formatting

--- deepsea_ai/tests/test_monitor.py
+++ deepsea_ai/tests/test_monitor.py
@@ -66,6 +66,7 @@ def test_remove_job():
     # clean up
     c.clear()
 
+
 def test_monitor_thread():
     c = JobCache(Path.cwd() / "tests" / "data" / "job_cache")
     job_name = "Dive1334"
@@ -74,8 +75,7 @@ def test_monitor_thread():
     # set the media
     c.set_media(job_name, "video1.mp4", JobStatus.SUCCESS)
     # creat a fake resource dict
-    resources = {}
-    resources["CLUSTER"] = "yolov5-benthic33k"
+    resources = {"CLUSTER": "yolov5-benthic33k"}
     # create a monitoring thread
     m = Monitor([job_name], resources)
     m.start()
