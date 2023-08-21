f2d6af31b89b30e51e44e7ae4c07743fa853293c
Duane Edgington <duane@mbari.org>
2023-08-21 13:50:09 -0700

    feat: add process command test

--- tests/test_process.py
+++ tests/test_process.py
@@ -9,7 +9,18 @@ from deepsea_ai.__main__ import cli
 video_path = Path(__file__).parent / 'data'
 
 
-@pytest.mark.exclude
-def test_some_feature():
-    # Your test code here
-    pass
+## @pytest.mark.exclude
+
+def test_process_args():
+    runner = CliRunner()
+    """Test that the process command works when passing arguments"""
+    args = '"--conf-thres=0.1 --iou-thres=0.4 --max-det=100 --agnostic-nms --save-vid --imgsz 640"'
+    result = runner.invoke(cli, ['process',
+                                 '--input', video_path.as_posix(),
+                                 '--job', 'Ventana Dive 4263 with 0.1 conf iou .4 with default',
+                                 '--input-s3', 's3://902005-dev-benchmark/',
+                                 '--model-s3', 's3://902005-dev-models/mbari-mb-benthic-315k/model.tar.gz',
+                                 '--output-s3', 's3://902005-dev-benchmarks-out/',
+                                 '--args', args])
+    # assert result.exit_code == 0
+    assert args in result.output
